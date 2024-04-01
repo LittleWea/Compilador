e374d099fcede8cd539c236090a6360c33ee4ef5
Ethienne117 <kirin1170v2@gmail.com>
2024-04-01 10:55:21 -0600

    Actualizacion

--- analLexico.py
+++ analLexico.py
@@ -3,9 +3,8 @@ import re
 def lexer(expresion):
     tokens = []
     # Definir patrones para los diferentes tokens
-    patron_palabra = r'\b[a-zA-Z_@][a-zA-Z0-9_@]*\b'
+    patron_palabra = r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
     patron_cadena_entre_dos_simbolos = r'##[^#]+##'
-    patron_com = r'#[^\n]*'
     patron_simbolo_aritmetico = r'[\+\-\*/\^\%]'
     patron_numero_real = r'[-+]?\d+\.\d+'
     patron_numero_entero = r'[-+]?\d+'
@@ -13,12 +12,14 @@ def lexer(expresion):
     patron_parentesis = r'[\(\)]'
     patron_corchetes = r'[\[\]]'
     patron_llaves = r'[\{\}]'
-    patron_punto = r'[\,\;]'
+    patron_punto = r'[\.\,\;]'
     patron_operadores_logicos = r'\|\||&&'
-    patron_objeto = r'\b[a-zA-Z_@]\.[a-zA-Z0-9_@]*\b'
 
     # Patrón general para ignorar saltos de línea y espacios
-    patron_ignorar = r'\s+'
+    patron_ignorar = r'[\n\s]+'
+
+    fila = 1
+    columna = 1
 
     # Combinar todos los patrones
     patron_general = '|'.join([
@@ -27,33 +28,36 @@ def lexer(expresion):
         patron_simbolo_aritmetico,
         patron_numero_real,
         patron_numero_entero,
-        patron_com,
         patron_simbolo_logico,
         patron_parentesis,
         patron_corchetes,
         patron_llaves,
         patron_punto,
         patron_operadores_logicos,
-        patron_objeto,
         patron_ignorar
     ])
 
     # Iterar sobre las coincidencias en la expresión
     for match in re.finditer(patron_general, expresion):
         token = match.group().strip()
+        columna = match.start() - expresion.rfind('\n', 0, match.start())
+        start_pos = match.start()
+        line = expresion.count('\n', 0, start_pos) + 1
+        # Actualizar la fila si hay un salto de línea
         # Descartar saltos de línea y espacios
+
         if token == '\n' or re.fullmatch(patron_ignorar, token):
             continue
-        tokens.append(token)
+        tokens.append((token, line, columna))
 
-    tokens = [elemento for elemento in tokens if elemento != '']
+    tokens = [elemento for elemento in tokens if elemento[0] != '']
 
     return tokens
 
-def tipoToken( expresion):
-    patron_palabra = r'\b[a-zA-Z_@][a-zA-Z0-9_@]*\b'
-    patron_com = r'#[^\n]*'
+def tipoToken(expresion):
+    patron_palabra = r'\b[a-zA-Z_][a-zA-Z0-9_@]*\b'
     patron_cadena_entre_dos_simbolos = r'##[^#]+##'
+    patron_com = r'#[^\n]*'
     patron_simbolo_aritmetico = r'[\+\-\*/\^\%]'
     patron_numero_real = r'[-+]?\d+\.\d+'
     patron_numero_entero = r'[-+]?\d+'
@@ -61,16 +65,29 @@ def tipoToken( expresion):
     patron_parentesis = r'[\(\)]'
     patron_corchetes = r'[\[\]]'
     patron_llaves = r'[\{\}]'
-    patron_objeto = r'\b[a-zA-Z_@]\.[a-zA-Z0-9_@]*\b'
     patron_punto = r'[\,\;]'
     patron_operadores_logicos = r'\|\||&&'
+
+    patron_general = '|'.join([
+            patron_cadena_entre_dos_simbolos,
+            patron_palabra,
+            patron_simbolo_aritmetico,
+            patron_numero_real,
+            patron_numero_entero,
+            patron_com,
+            patron_simbolo_logico,
+            patron_parentesis,
+            patron_corchetes,
+            patron_llaves,
+            patron_punto,
+            patron_operadores_logicos
+        ])
+
     if( (expresion == 'if') | (expresion == 'othewise') | (expresion == 'do') | (expresion == 'while') | 
        (expresion == 'switch') | (expresion == 'case') | (expresion == 'integer') | (expresion == 'double') | 
-       (expresion == 'main') | (expresion == 'return')|(expresion == 'else')):
+       (expresion == 'main') | (expresion == 'return') | (expresion == 'else') | (expresion == 'cin') | (expresion == 'cout')):
         return 'palabra reservada'
     elif(re.fullmatch(patron_cadena_entre_dos_simbolos, expresion)):
-        return 'comentario'
-    elif(re.fullmatch(patron_com, expresion)):
         return 'comentario'
     elif(re.fullmatch(patron_palabra, expresion)):
         return 'identificador'
@@ -90,7 +107,13 @@ def tipoToken( expresion):
         return 'simbolo llave'
     elif(re.fullmatch(patron_punto, expresion)):
         return 'simbolo puntuacion'
-    elif(re.fullmatch(patron_operadores_logicos, expresion)):
-        return 'operador logico'
-    elif(re.fullmatch(patron_objeto, expresion)):
-        return 'objeto con funcion'
+    elif (re.fullmatch(patron_general, expresion) == None):
+        return 'error'
+    
+lexer("""Tú me quieres alba,
+me quieres de espumas,
+me quieres de nácar.
+Que sea azucena
+Sobre todas, casta.
+De perfume tenue.
+Corola cerrada .""")
